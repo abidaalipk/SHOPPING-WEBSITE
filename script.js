d1d79febb5606450022f46d2ff349a5936428249
abidaalipk <152191195+abidaalipk@users.noreply.github.com>
2024-03-16 08:32:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var product_total_amt = document.getElementById('product_total_amt');
+var shipping_charge = document.getElementById('shipping_charge');
+var total_cart_amt = document.getElementById('total_cart_amt');
+var discountCode = document.getElementById('discount_code1');
+
+const decreaseNumber = (incdec, itemprice) => {
+   var itemval = document.getElementById(incdec);
+   var itemprice = document.getElementById(itemprice);
+    // console.log(itemval.value);
+
+      if(itemval.value < 0){
+        itemval.value = 0;
+        alert('Negtive value not allowed');
+      }else{
+        itemval.value = parseInt(itemval.value) - 1;
+        itemval.style.background = '#fff';
+        itemval.style.color = '#000';
+        itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
+        product_total_amt.innerHTML = parseInt(product_total_amt.innerHTML) - 15;
+        total_cart_amt.innerHTML = parseInt(product_total_amt.innerHTML) + parseInt(shipping_charge.innerHTML);
+        
+      }
+
+}
+
+const increaseNumber = (incdec, itemprice) => {
+    var itemval = document.getElementById(incdec);
+    var itemprice = document.getElementById(itemprice);
+     // console.log(itemval.value);
+ 
+       if(itemval.value > 10 ){
+         itemval.value = 10;
+         alert('max 10 allowed');
+         itemval.style.background = 'red';
+         itemval.style.color = '#fff';
+       }else{
+         itemval.value = parseInt(itemval.value) + 1;
+         itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
+         product_total_amt.innerHTML = parseInt(product_total_amt.innerHTML) + 15;
+         total_cart_amt.innerHTML = parseInt(product_total_amt.innerHTML) + parseInt(shipping_charge.innerHTML);
+       }
+ }
+
+ const  discount_code = () => {
+     let totalAmtCurr = parseInt(total_cart_amt.innerHTML);
+     let error_trw = document.getElementById('error_trw');
+     if(discountCode.value === "Abida"){
+        let newtotalamt = totalAmtCurr - 15;
+        total_cart_amt.innerHTML = newtotalamt;
+        error_trw.innerHTML = "Hurray!  Code is Valid ";
+     }else{
+        error_trw.innerHTML = "TryAgain! Valid Code is Abida ";
+     }
+ }
